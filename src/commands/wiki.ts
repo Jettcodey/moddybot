9032d10ec774fd28d189c80618af804999948771
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2026-02-12 11:46:30 -0600

    A

--- src/commands/wiki.ts
+++ src/commands/wiki.ts
@@ -51,7 +51,7 @@ const buildUrl = (path: string): string => {
 export default {
     data: new SlashCommandBuilder()
         .setName('wiki')
-        .setDescription('Forwards to a wiki of REPO modding.')
+        .setDescription('Replies to a wiki of REPO modding.')
         .addStringOption(option => option
             .setName('page')
             .setDescription('The wiki page to view')
