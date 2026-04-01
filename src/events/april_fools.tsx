cf5daf4040fef8c85feb6b64b4102855bb9453bc
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2026-04-01 10:52:04 -0500

    Whoops

--- src/events/april_fools.tsx
+++ src/events/april_fools.tsx
@@ -10,13 +10,13 @@ export default {
     async execute(member: GuildMember) {
         if (member.guild.id == "1344557689979670578")
         {
+            LEFT_COUNT++;
             const alertChannel = member.guild.channels.cache.get('1344557689979670582') as TextChannel;
             if (alertChannel) {
                 await alertChannel.send({
                     content: `${member.user.username} (${member.id}) has left the server from possibly the april fools joke :). count is now ${LEFT_COUNT}`,
                 });
             }
-            LEFT_COUNT++;
         }
     }
 }
