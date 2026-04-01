3ebe049702061367987af626f97031f79e5435cf
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2026-04-01 13:50:24 -0500

    Update april_fools.tsx

--- src/events/april_fools.tsx
+++ src/events/april_fools.tsx
@@ -4,7 +4,7 @@ import {getGuildConfig, setGuildConfig} from "@/utils/config.ts";
 const GUILD_ID = "1344557689979670578";
 const ALERT_CHANNEL_ID = "1344557689979670582";
 
-let leftCount = getGuildConfig(GUILD_ID).left_count ?? 83;
+let leftCount = getGuildConfig(GUILD_ID).left_count ?? 134;
 
 export default {
     name: "guildMemberRemove",
@@ -12,11 +12,9 @@ export default {
         if (member.guild.id === GUILD_ID) {
             leftCount++;
             const alertChannel = member.guild.channels.cache.get(ALERT_CHANNEL_ID) as TextChannel;
-            if (alertChannel) {
-                await alertChannel.send({
-                    content: `${member.user.username} (${member.id}) has left the server from possibly the april fools joke :). count is now ${leftCount}`,
-                });
-            }
+            await alertChannel.send({
+                content: `${member.user.username} (${member.id}) has left the server from possibly the april fools joke :). count is now ${leftCount}`,
+            });
             setGuildConfig(member.guild.id, {left_count: leftCount});
         }
     }
